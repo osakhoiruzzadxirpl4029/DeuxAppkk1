8ae3ddafd582cb8d255a81c76dfc6e2199d85c9a
BimaPutra34 <putrabima378@gmail.com>
2016-11-22 07:14:37 +0700

    Tambahan Sumatera

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl402112029/deuxappkk/SumateraLayout.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl402112029/deuxappkk/SumateraLayout.java
@@ -1,17 +1,71 @@
 package id.sch.smktelkom_mlg.project.xirpl402112029.deuxappkk;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.project.xirpl402112029.deuxappkk.adapter.HotelAdapter;
+import id.sch.smktelkom_mlg.project.xirpl402112029.deuxappkk.model.Hotel;
 
 /**
  * Created by SUPER USER on 20/11/2016.
  */
 
 public class SumateraLayout extends AppCompatActivity {
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.sumatera_layout);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.sumatera_places);
+        String[] arDeskripsi = resources.getStringArray(R.array.sumatera_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.sumatera_pictures);
+        Drawable[] arFoto = new Drawable[a.length()];
+        for (int i = 0; i < arFoto.length; i++) {
+            BitmapDrawable bd = (BitmapDrawable) a.getDrawable(i);
+            RoundedBitmapDrawable rbd = RoundedBitmapDrawableFactory.create(getResources(), bd.getBitmap());
+            rbd.setCircular(true);
+            arFoto[i] = rbd;
+        }
+        a.recycle();
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Hotel(arJudul[i], arDeskripsi[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
