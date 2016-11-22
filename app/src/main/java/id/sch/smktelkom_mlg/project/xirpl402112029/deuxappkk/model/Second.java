19e9a3d457ece4d5ffe4b98ec3499bd2f2fb51f2
BimaPutra34 <putrabima378@gmail.com>
2016-11-22 09:28:35 +0700

    Tambahan code button sulawesi

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl402112029/deuxappkk/model/Second.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.project.xirpl402112029.deuxappkk.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by AS X550Z on 22/11/2016.
+ */
+
+public class Second {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Second(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
